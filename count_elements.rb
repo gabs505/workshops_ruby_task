902bb928fe48ed428f816368fae1f44fbb833c90
gabi <gawieliczko@gmail.com>
2023-10-04 18:29:44 +0200

    added solution for tasks 1 and 2

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,18 @@
 class CountElements
+  def initialize(args)
+    @args = args
+  end
+
+  def perform
+    result = {}
+    @args.each do |el|
+      if result[el].nil?
+        result[el] = 1
+      else
+        result[el] += 1
+      end
+    end
+    result
+  end
 
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,10 @@
 class ValidatePassword
 
+  def initialize(password)
+    @password = password
+  end
+
+  def perform
+    !((@password =~ /^(?!.*(.)\1{2,})(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%&*+=:;?<>]).{6,24}$/).nil?)
+  end
 end
